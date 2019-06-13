8bce7eae0c15213c1c6fd5635eae4d79836c6f7d
Keith Gan <keithgan96@gmail.com>
2019-06-13 17:38:18 +0800

    tidied code

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    protected $fillable = ['post_id', 'name', 'date', 'start_time', 'end_time', 'location', 'description', 'fee', 'fee_amount'];
+    protected $fillable = [
+        'post_id', 'name', 'date', 'start_time', 'end_time', 'location', 'description', 'fee', 'fee_amount'
+    ];
 }
