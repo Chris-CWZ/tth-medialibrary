bccd0df46f930ebcccaff802cf1c64763b960977
Madeleine Cheng Mei Tee <madeleinetee@hotmail.com>
2019-07-11 17:32:58 +0800

    Implemented ProductServices methods

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -9,11 +9,23 @@ class ProductService {
     /**
 	*
 	*	Retrieve products
-	*	Request input: sort/filter(optional)
+    *	Request input: sort, order, filter, min, max
+    *   Default: sort by latest
 	*
 	**/
     public function retrieveProductsList($request){
-        
+        $sort = $request->sort ? $request->sort : 'created_at';
+        $order = $request->order ? $request->order : 'desc';
+
+        $query = $products = Product::where('vendor', 'trp')->orderBy($sort, $order)->limit(10);
+
+        if ($request->has('filter')) {
+            $products = $query->whereBetween($request->filter, [$request['min'], $request['max']])->get();
+        } else{
+            $products = $query->get();
+        }
+
+        return respond($products);
     }
 
     /**
@@ -24,5 +36,37 @@ class ProductService {
     public function retrieveProduct($cartProduct){
         $product = Product::where('id', $cartProduct['product_id'])->first();
         return $product;
+    }
+
+    /**
+	*
+    *	Retrieve a particular product's colour
+    *   Request: product_code
+	*
+	**/
+    public function colours($request){
+        $colours = Product::select('colour')->where('product_code', $request['product_code'])->distinct()->get();
+        
+        foreach($colours as $colour) {
+            $coloursArray[] = $colour['colour'];
+        }
+
+        return respond($coloursArray);
+    }
+
+    /**
+	*
+    *	Retrieve a particular product's size
+    *   Request: product_code
+	*
+	**/
+    public function sizes($request){
+        $sizes = Product::select('size')->where('product_code', $request['product_code'])->distinct()->get();
+        
+        foreach($sizes as $size) {
+            $sizesArray[] = $size['size'];
+        }
+
+        return respond($sizesArray);
     }
 }
