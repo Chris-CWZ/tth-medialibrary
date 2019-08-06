0759ca157d05f7ead8db9c953096db418187b998
Byron <byrontan92@gmail.com>
2019-08-06 15:22:22 +0800

    edited article migration table

--- database/migrations/2019_08_06_144043_create_articles_table.php
+++ database/migrations/2019_08_06_144043_create_articles_table.php
@@ -15,6 +15,11 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->string('location')->nullable();
+            $table->longtext('body')->nullable();
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
             $table->timestamps();
         });
     }
