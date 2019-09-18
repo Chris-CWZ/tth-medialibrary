d6be53e72d04cf9e576952aaaf3971bb4b6a190a
Keith Gan <keithgan96@gmail.com>
2019-09-18 14:08:42 +0800

    fixed events query

--- database/migrations/2019_08_06_144448_create_file_element_article_table.php
+++ database/migrations/2019_08_06_144448_create_file_element_article_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFileElementTable extends Migration
+class CreateFileElementArticleTable extends Migration
 {
     /**
      * Run the migrations.
